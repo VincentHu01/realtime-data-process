7efaefd9755666ced8984e1503e9c7b9fe3c5084
Jason <hzs106@126.com>
2019-01-06 23:41:40 +0800

    success demo

--- src/main/java/com/ai/spark/SparkStreaming.scala
+++ src/main/java/com/ai/spark/SparkStreaming.scala
@@ -1,12 +1,21 @@
 package com.ai.spark
 
-import java.util.Properties
+import java.util.{Arrays, Properties}
 
 import com.ai.utils.PropUtil
+import com.google.common.primitives.Bytes
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.spark._
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import kafka.serializer.{DefaultDecoder, StringDecoder}
+import kafka.utils.ZkUtils
+import org.apache.hadoop.hbase.protobuf.generated.ClientProtos.Scan
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.kafka.common.TopicPartition
+import org.spark_project.jetty.server.HttpConfiguration.ConnectionFactory
+
 
 /**
   * Created by Jason on 2018/12/28.
@@ -16,36 +25,19 @@ object SparkStreaming {
   private val prop = PropUtil.getProps("kafka.properties")
 
   def run(): Unit = {
-    val conf = new SparkConf().setMaster("local[4]").setAppName("NetworkWordCount")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
     val ctx = new SparkContext(conf)
     ctx.setLogLevel("WARN")
     val ssc = new StreamingContext(ctx, Seconds(5))
     //var zookeeper = "ip:2181,ip:2182".replaceAll("ip",prop.getProperty("HOST_IP"))
-    val zookeeper =  "PLAINTEXT://192.112.130.181:9092"
-    println("zookeeper: "+ zookeeper)
-
-    val props: Properties = new Properties
-    props.put("bootstrap.servers", "193.112.130.181:9092")
-    props.put("group.id", "test")
-    props.put("enable.auto.commit", "true")
-    props.put("auto.commit.interval.ms", "1000")
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-
-//    val kafkaParams = Map[String,Object](
-//      "bootstrap.servers" -> zookeeper,
-//      //"broker.list"->"master:9092",
-//      "key.deserializer" -> classOf[StringDeserializer],
-//      "value.deserializer" -> classOf[StringDeserializer],
-//      //"group.id" -> "group1",
-//      "auto.offset.reset" -> "latest",
-//      "enable.auto.commit" -> (false: java.lang.Boolean)
-//    )
+    val brokers:String = "ip:9092,ip:9093".replaceAll("ip",prop.getProperty("HOST_IP"))
+    println("brokers: "+ brokers)
 
     val kafkaParams2 = Map[String,Object](
-      "bootstrap.servers" -> "193.112.130.181:9092",
+      "bootstrap.servers" -> brokers,
       "group.id"-> "test",
-      "enable.auto.commit"-> "true",
+      "enable.auto.commit"-> (true: java.lang.Boolean),
+      "auto.offset.reset" -> "earliest",
       "auto.commit.interval.ms"->"1000",
       "key.deserializer"->"org.apache.kafka.common.serialization.StringDeserializer",
       "value.deserializer"->"org.apache.kafka.common.serialization.StringDeserializer"
@@ -56,21 +48,23 @@ object SparkStreaming {
     topics.foreach(println)
 
     val consumer = ConsumerStrategies.Subscribe[String, String](topics, kafkaParams2)
-    val messages = KafkaUtils.createDirectStream(
+    val messages = KafkaUtils.createDirectStream[String,String](
       ssc,
       LocationStrategies.PreferConsistent,
       consumer
     )
+    //messages.foreachRDD(rdd=>println(rdd.count()))
 
-    val lines=messages.map(record => record.value)
+    val lines = messages.map(record=> record.value)
     val wordCounts = lines.map(x => {
       (x,1)
     }).reduceByKey(_ + _)
-
     wordCounts.print()
+
     ssc.start()
     ssc.awaitTermination()
   }
+
 
   def main(args: Array[String]): Unit ={
     run()
